f36816e56a4a358b51eeb91a7f0220d17dc1d10a
yama-yeah <b1021091@fun.ac.jp>
2023-07-13 21:41:40 +0900

    [fix] 例外の処理漏れを修正

--- app/src/main/kotlin/jp/co/yumemi/android/code_check/domain/services/github/GithubApi.kt
+++ app/src/main/kotlin/jp/co/yumemi/android/code_check/domain/services/github/GithubApi.kt
@@ -1,5 +1,6 @@
 package jp.co.yumemi.android.code_check.domain.services.github
 
+import android.util.Log
 import io.ktor.client.HttpClient
 import io.ktor.client.call.body
 import io.ktor.client.engine.android.Android
@@ -13,15 +14,21 @@ import org.json.JSONObject
 class GithubApiImpl : GitHubApi {
     private val client = HttpClient(Android)
     override suspend fun getRepositoriesJson(repositoryName: String): JSONArray? {
-        val response: HttpResponse = client.get("https://api.github.com/search/repositories") {
-            header("Accept", "application/vnd.github.v3+json")
-            parameter("q", repositoryName)
+        return try {
+            val response: HttpResponse = client.get("https://api.github.com/search/repositories") {
+                header("Accept", "application/vnd.github.v3+json")
+                parameter("q", repositoryName)
+            }
+
+            val jsonBody = JSONObject(response.body<String>())
+
+            // jsonからリポジトリたちを取得
+            return jsonBody.optJSONArray("items")
+        } catch (e: Exception) {
+            Log.e("GitHubApiとの通信でエラー", e.toString())
+            null
         }
 
-        val jsonBody = JSONObject(response.body<String>())
-
-        // jsonからリポジトリたちを取得
-        return jsonBody.optJSONArray("items")
     }
 }
 
